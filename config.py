f012d769a2cd1e9f28d8c9f632fc6a170ed01590
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-01-23 19:25:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,30 @@
-config = {# models
-          'c620_model_path':'model/c620.pkl',
-          'c660_model_path':'model/c660.pkl',
-          'c670_model_path':'model/c670.pkl',
+config = {
+      # model
+      'icg_model_path':'model/c620_icg.pkl',
+      'c620_model_path':'model/c620.pkl',
+      'c660_model_path':'model/c660.pkl',
+      'c670_model_path':'model/c670.pkl',
+      
+      # col_names
+      'icg_col_path':'col_names/c620_c670.pkl',
+      'c620_col_path':'col_names/c620_col_names.pkl',
+      'c660_col_path':'col_names/c660_col_names.pkl',
+      'c670_col_path':'col_names/c670_col_names.pkl',
           
-          # col_names
-          'c620_col_path':'col_names/c620_col_names.pkl',
-          'c660_col_path':'col_names/c660_col_names.pkl',
-          'c670_col_path':'col_names/c670_col_names.pkl',
+      # Special column (0.9999 & 0.0001)
+      'index_9999_path':'other/index_9999.pkl',
+      'index_0001_path':'other/index_0001.pkl',
           
-          # icg input and output
-          'icg_log_path':'log/icg_log.xlsx',
-
-          # f input
-          'f_input_log_path':'log/f_input_log.xlsx',
+      # input log Excel file
+       'input_log_path':'log/input_log.xlsx',
+      
+      # wt log Excel file
+       'c620_wt_log_path':'log/c620_wt_log.xlsx',
+       'c660_wt_log_path':'log/c660_wt_log.xlsx',
+       'c670_wt_log_path':'log/c670_wt_log.xlsx',
           
-          # wt
-          'c620_wt_log_path':'log/c620_wt_log.xlsx',
-          'c660_wt_log_path':'log/c660_wt_log.xlsx',
-          'c670_wt_log_path':'log/c670_wt_log.xlsx',
-          
-          # op
-          'c620_op_log_path':'log/c620_op_log.xlsx',
-          'c660_op_log_path':'log/c660_op_log.xlsx',
-          'c670_op_log_path':'log/c670_op_log.xlsx',
+      # op log Excel file
+       'c620_op_log_path':'log/c620_op_log.xlsx',
+       'c660_op_log_path':'log/c660_op_log.xlsx',
+       'c670_op_log_path':'log/c670_op_log.xlsx',
           }
-
-
