220979ac3e8ddde925268b1daa79aa311dc8169d
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-04-05 00:46:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,9 @@ config = {
       'c620_model_path_real_data':'model/c620_real_data.pkl',
       'c660_model_path_real_data':'model/c660_real_data.pkl',
       'c670_model_path_real_data':'model/c670_real_data.pkl',
+      
+      # real data op model path
+      'c620_model_path_real_data_op':'model/c620_op_real_data.pkl'
 
       # col_names
       'icg_col_path':'col_names/c620_c670.pkl',
