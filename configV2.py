20a87c789cf8aa49b0c681a1bf43b3592ffc22d7
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-04-20 12:21:26 +0800

    Create configV2.py

---
+++ configV2.py
@@ -0,0 +1,31 @@
+config = {
+      # c620
+      'c620_G':'model/c620_G.pkl',
+      'c620_F':'model/c620_F.pkl',
+      'c620_op_min':'model/c620_op_min.pkl',
+      'c620_op_max':'model/c620_op_max.pkl',
+
+      # c660
+      'c660_G':'model/c660_G.pkl',
+      'c660_F':'model/c660_F.pkl',
+      'c660_op_min':'model/c660_op_min.pkl',
+      'c660_op_max':'model/c660_op_max.pkl',
+
+      # c670
+      'c670_M':'model/c670.pkl',
+      
+      # col_names
+      'icg_col_path':'col_names/icg_col_names.pkl',
+      'c620_col_path':'col_names/c620_col_names.pkl',
+      'c660_col_path':'col_names/c660_col_names.pkl',
+      'c670_col_path':'col_names/c670_col_names.pkl',
+      
+      # Special column (0.9999 & 0.0001)
+      'index_9999_path':'data/index_9999.pkl',
+      'index_0001_path':'data/index_0001.pkl',
+
+      # sp
+      'c620_wt_always_same_split_factor_dict':'data/c620_wt_always_same_split_factor_dict.pkl',
+      'c660_wt_always_same_split_factor_dict':'data/c660_wt_always_same_split_factor_dict.pkl',
+      'c670_wt_always_same_split_factor_dict':'data/c670_wt_always_same_split_factor_dict.pkl',
+          }
