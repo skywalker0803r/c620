fc73deb5fce4a31ac335139972ca9642ee8f7695
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-04-23 11:08:54 +0800

    Update FV2.py

--- FV2.py
+++ FV2.py
@@ -263,7 +263,7 @@ class AllSystem(object):
       return loss1 + loss2
     
     # NA in Benzene_ppmw 從使用者輸入的數值當作初始值開始調整
-    x0 = {'Simulation Case Conditions_Spec 2 : NA in Benzene_ppmw'=icg_input['Simulation Case Conditions_Spec 2 : NA in Benzene_ppmw'].values[0]}
+    x0 = {'Simulation Case Conditions_Spec 2 : NA in Benzene_ppmw':icg_input['Simulation Case Conditions_Spec 2 : NA in Benzene_ppmw'].values[0]}
     
     # cma-es 優化
     sampler = optuna.samplers.CmaEsSampler(x0=x0)
