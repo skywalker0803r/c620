622f621bd0fb422de853f128a0d75dbf0dd3ea0b
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-05-13 09:38:52 +0800

    Update FV2.py

--- FV2.py
+++ FV2.py
@@ -200,7 +200,7 @@ class AllSystem(object):
      history['nainbz'] = []
      step = 0.05
      for dist_rate in tqdm(np.arange(0,10,step)):
-        icg_input2 = demo['icg_input'].copy()
+        icg_input2 = icg_input.copy()
         icg_input2['Tatoray Stripper C620 Operation_Specifications_Spec 2 : Distillate Rate_m3/hr'] = dist_rate
         c620_wt2,c620_op2,c660_wt2,c660_op2,c670_wt2,c670_op2,c660_mf2 = self.inference(icg_input2.copy(),
                                                                                         c620_feed.copy(),
